8eba00b964a0f437abbcd88a8daa026c6a95086e
Nestor Lenaerts <nestor.lenaerts@gmail.com>
2022-02-06 14:57:01 +0100

    nog niet aff

--- Week2/Program.cs
+++ Week2/Program.cs
@@ -7,6 +7,20 @@ namespace Week2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //deze heb ik zelf niet gedaan, ik had half zonder de if().
+            Console.WriteLine("Geef je verjaardag (formaat: d/m . Bv 18/3)");
+            DateTime verj = DateTime.Parse(Console.ReadLine());
+
+
+            if (verj < DateTime.Today)
+                verj = verj.AddYears(1);
+
+
+            string dagLokaal = System.Globalization.DateTimeFormatInfo.CurrentInfo.GetDayName(verj.DayOfWeek);
+            TimeSpan dagenOver = verj - DateTime.Today;
+
+            Console.WriteLine($"Je ben jarig over {dagenOver.Days} dagen en dat is op een {dagLokaal}.");
         }
     }
 }
