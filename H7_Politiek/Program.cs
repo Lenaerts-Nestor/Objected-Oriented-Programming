90b25a1d9ec317c123ecaa2260f00ea2b74c4417
Nestor Lenaerts <nestor.lenaerts@gmail.com>
2022-05-09 21:34:24 +0200

    UPDATE 21:34

---
+++ H7_Politiek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace H7_Politiek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            
+        }
+    }
+}
